5de3e298b319aae3fee54d33afed6f210020ac64
emmanuel.nuyttens <emmanuel.nuyttens@outlook.com>
2020-02-16 12:22:20 +0100

    INotifier interface commit local

---
+++ src/Custom/Mediator.Logic/INotifier.cs
@@ -0,0 +1,13 @@
+﻿namespace Mediator.Logic
+{
+    /// <summary>
+    /// Author      : Emmanuel Nuyttens
+    /// Date        : 02-2020
+    /// Purpose     : Mediator notifier interface
+    /// </summary>
+    public interface INotifier
+    {
+        void Notify();
+        bool CanRun();
+    }
+}
